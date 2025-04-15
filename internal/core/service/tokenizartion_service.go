4dd097a505d13dde50b10b459ba35a150a3958b4
eliezerraj <eliezerraj@gmail.com>
2025-04-14 23:45:19 -0300

    fix:otel

---
+++ internal/core/service/tokenizartion_service.go
@@ -0,0 +1,48 @@
+package service
+
+import(
+	"context"
+
+	"github.com/go-tokenization-grpc/internal/adapter/database"
+	"github.com/go-tokenization-grpc/internal/core/model"
+	"github.com/rs/zerolog/log"
+
+	go_core_observ "github.com/eliezerraj/go-core/observability"
+)
+
+var childLogger = log.With().Str("component","go-tokenization-grpc").Str("package","internal.core.service").Logger()
+var tracerProvider go_core_observ.TracerProvider
+
+type WorkerService struct {
+	apiService		[]model.ApiService
+	workerRepository *database.WorkerRepository
+}
+
+// About create a new worker service
+func NewWorkerService(	workerRepository *database.WorkerRepository,
+						apiService		[]model.ApiService) *WorkerService{
+
+	childLogger.Info().Str("func","NewWorkerService").Send()
+
+	return &WorkerService{
+		workerRepository: workerRepository,
+		apiService: apiService,
+	}
+}
+
+// About get the card from token
+func (s * WorkerService) GetCardToken(ctx context.Context, card model.Card) (*[]model.Card, error){
+	childLogger.Info().Str("func","GetCardToken").Interface("trace-request-id", ctx.Value("trace-request-id")).Interface("card", card).Send()
+
+	// Trace
+	span := tracerProvider.Span(ctx, "service.GetCardToken")
+	defer span.End()
+
+	// Get card token information from repo
+	res, err := s.workerRepository.GetCardToken(ctx, card)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
